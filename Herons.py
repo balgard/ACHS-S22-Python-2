cc8f98ce14e2e5a5133a99f8092ffd0af560f336
Brendan Algard <brendan.algard@gmail.com>
2022-01-27 12:24:53 -0600

    Update Herons.py

--- Herons.py
+++ Herons.py
@@ -4,6 +4,8 @@ side_a = float(input("Enter a number for side A: "))
 side_b = float(input("Enter a number for side B: "))
 side_c = float(input("Enter a number for side C: "))
 
+#Change
+
 semiperimeter = (side_a + side_b + side_c) / 2
 
 if semiperimeter - a < 0 or semiperimeter - b < 0 or semiperimeter - c < 0:
